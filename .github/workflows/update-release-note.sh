00b9a4bbae8ea1d1cc3fcdc093acc4880f617486
Remi Cattiau <remi@cattiau.com>
2023-06-23 17:59:58 -0700

    build: update-release-note with workspace snippet

---
+++ .github/workflows/update-release-note.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e
+
+# For mac
+if ! [ -x "$(command -v sha256sum)" ]; then
+  alias sha256sum="shasum -a256"
+fi
+
+gh release view ${TAG} --json body -q .body > release.txt
+DIGEST=`wget https://github.com/loopingz/rules_k8s_cd/archive/refs/tags/${TAG}.tar.gz -O- | sha256sum | cut -f1 -d' '`
+cat << EOF >> release.txt
+
+## Workspace snippet
+
+\`\`\`starlark
+load("@bazel_tools//tools/build_defs/repo:http.bzl", "http_archive")
+
+http_archive(
+    name = "com_loopingz_rules_k8s_cd",
+    sha256 = "${DIGEST}",
+    strip_prefix = "rules_k8s_cd-${TAG}",
+    urls = ["https://github.com/loopingz/rules_k8s_cd/archive/refs/tags/${TAG}.tar.gz"],
+)
+\`\`\`
+
+
+EOF
+gh release edit ${TAG} -F release.txt
